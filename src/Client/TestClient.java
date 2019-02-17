e00be5b4a2e7da8eadf863c43e2c6ab8cb20112f
katanaWieldingPanda <Rico0Sandard@gmail.com>
2019-02-17 18:12:20 -0500

    Add peer-to-peer connection. Up next: Multi-Client

---
+++ src/Client/TestClient.java
@@ -0,0 +1,10 @@
+package Client;
+
+public class TestClient {
+
+	public static void main(String[] args) {
+		Client cli = new Client ("127.0.0.1");
+		cli.startRunning();
+	}
+
+}
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -2,89 +2,88 @@ package Server;
 
 import java.io.*;
 import java.net.*;
+import javax.swing.*;
 
 public class Server {
-
-	private ServerSocket server;
-	private Socket connection;
+	
+	private JTextField userText;
+	private JTextArea chatWindow;
 	private ObjectOutputStream output;
 	private ObjectInputStream input;
+	private ServerSocket server;
+	private Socket connection;
+		
+
 	
-	public void startRunning () {
-		try {
-			
-			server = new ServerSocket (8080);
-			while (true) {
-				try {
+	public void startRunning(){        
+		try{
+			server = new ServerSocket(6789, 100); //6789 is a dummy port for testing, this can be changed. The 100 is the maximum people waiting to connect.
+			while(true){
+				try{
+					//Trying to connect and have conversation
 					waitForConnection();
 					setupStreams();
 					whileChatting();
-				}catch(EOFException eoException) {
-					System.out.println("Server ended the connection!");
-				} finally {
-					closeSocket();
+				}catch(EOFException eofException){
+					System.out.println("\n Server ended the connection! ");
+				} finally{
+					closeConnection(); //Changed the name to something more appropriate
 				}
 			}
-			
-		}catch(IOException ioException) {
+		} catch (IOException ioException){
 			ioException.printStackTrace();
 		}
 	}
-	
-	//Wait for connection
-	private void waitForConnection () throws IOException {
-		System.out.println("Waiting for someone to connect...");
+	//wait for connection, then display connection information
+	private void waitForConnection() throws IOException{
+		System.out.println(" Waiting for someone to connect... \n");
 		connection = server.accept();
-		System.out.println("Now Connected to " + connection.getInetAddress().getHostName());
+		System.out.println(" Now connected to " + connection.getInetAddress().getHostName());
 	}
 	
-	//Send and Receive Data
-	private void setupStreams() throws IOException {
-		output = new ObjectOutputStream (connection.getOutputStream());
+	//get stream to send and receive data
+	private void setupStreams() throws IOException{
+		output = new ObjectOutputStream(connection.getOutputStream());
 		output.flush();
 		
-		input = new ObjectInputStream (connection.getInputStream());
-		System.out.println("Streams are now setup!");
+		input = new ObjectInputStream(connection.getInputStream());
+		
+		System.out.println("\n Streams are now setup \n");
 	}
 	
-	//During the chat
+	//during the chat conversation
 	private void whileChatting() throws IOException{
-		String message = "You are now connected!";
+		String message = " You are now connected! ";
 		sendMessage(message);
-		
-		do {
-			try {
-				
+		do{
+			try{
 				message = (String) input.readObject();
-				System.out.println(message);
-				
-			}catch(ClassNotFoundException classNotFoundException) {
-				System.out.println("Error at whileChatting().");
+				System.out.println("\n" + message);
+			}catch(ClassNotFoundException classNotFoundException){
+				System.out.println("The user has sent an unknown object!");
 			}
 		}while(!message.equals("CLIENT - END"));
 	}
 	
-	//close streams and sockets after you are done chatting
-	private void closeSocket() {
-		System.out.println("...Closing Connections...");
-		try {
-			output.close();
-			input.close();
-			connection.close();
-		}catch(IOException ioException) {
+	public void closeConnection(){
+		System.out.println("\n Closing Connections... \n");
+		try{
+			output.close(); //Closes the output path to the client
+			input.close(); //Closes the input path to the server, from the client.
+			connection.close(); //Closes the connection between you can the client
+		}catch(IOException ioException){
 			ioException.printStackTrace();
 		}
 	}
 	
-	//send a message to client
-	private void sendMessage (String msg) {
-		try {
-			output.writeObject("Server - " + msg);
+	//Send a message to the client
+	private void sendMessage(String message){
+		try{
+			output.writeObject("SERVER - " + message);
 			output.flush();
-			System.out.println("Server - " + msg);
-		}catch(IOException ioException) {
-			System.out.println("Error at sendMessage()");
+			System.out.println("\nSERVER -" + message);
+		}catch(IOException ioException){
+			chatWindow.append("\n ERROR: CANNOT SEND MESSAGE, PLEASE RETRY");
 		}
 	}
-
 }
