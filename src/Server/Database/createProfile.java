ad803f9c8b373acfc7f7fe831edbfbe883a41bcd
Liangyu Zhao <Liangyu.zhao@uoit.net>
2019-03-22 18:44:30 -0400

    what the heck?

--- src/Server/Database/createProfile.java
+++ src/Server/Database/createProfile.java
@@ -8,25 +8,30 @@ import DB.User;
 
 public class createProfile {
 
-    public static void main(String args[]) {
-
+    public static boolean createProfile(String email, String password){
         createProfile objectIO = new createProfile();
 
-        User user = new User("9752@qq.com","zla");//should get information by here
+        User user = new User(email,password);//should get information by here
         objectIO.WriteObjectToFile(user.getemail(),user);
+        if (checkProfile(user.getemail()) == true){
+            return true;
+        }
+        return false;
     }
 
-    public static boolean checkexist(String email){
+    public static boolean checkProfile(String email){
         File tmp = new File(email);
         boolean exist = tmp.exists();
         return exist;
     }
-    public static boolean WriteObjectToFile(String fileName, User user) {
+
+
+    public static void WriteObjectToFile(String fileName, User user) {
 
         try {
-            if (checkexist(fileName) == true){
+            if (checkProfile(fileName) == true){
                 System.out.println("user data already exist");
-                return false;
+                return;
             }
             File f = new File(fileName);
             ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f));
@@ -34,11 +39,9 @@ public class createProfile {
             oos.flush();
             oos.close();
             System.out.println("The Object  was succesfully written to a file");
-            return true;
 
         } catch (Exception ex) {
             ex.printStackTrace();
-            return false;
         }
     }
 
